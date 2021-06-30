a29b0d6736da6edb36a0f3dfdd9ffb1cc53ce368
Maksym <mdundukov@gmail.com>
2021-06-30 14:25:56 +0300

    update main-page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+export interface Movie {
+
+}
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+  public baseUrl: string = 'https://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&language=en-US';
+  public totalDB: any = [];
+  public moviesDB: any = [];
+  public imgSrc: string = 'https://image.tmdb.org/t/p/w300';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
+    this.http.get<void>(this.baseUrl).subscribe(res => {
+      this.totalDB = res;
+      this.moviesDB = this.totalDB.results;
+    })
   }
+
 
 }
