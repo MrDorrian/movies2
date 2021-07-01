66a226c10ef8b06c9ffecc1d0b5476f43cfe00f9
Maksym <mdundukov@gmail.com>
2021-07-02 00:29:53 +0300

    add func detail favorite movie

--- src/app/favorite-page/favorite-page.component.ts
+++ src/app/favorite-page/favorite-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-favorite-page',
@@ -9,7 +10,7 @@ export class FavoritePageComponent implements OnInit {
   public favoriteListMovies: any[] = [];
   public imgSrc: string = 'https://image.tmdb.org/t/p/w300';
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
@@ -18,5 +19,12 @@ export class FavoritePageComponent implements OnInit {
 
   loadFavoriteList() {
     this.favoriteListMovies = JSON.parse(<string>localStorage.getItem('favorite'));
+  }
+
+  detailMovie(e: any) {
+    if (e.target.id) {
+      this.router.navigate(['/detail', e.target.id])
+    }
+    return
   }
 }
