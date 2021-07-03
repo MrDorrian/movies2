49c9f458a4f8495d1021cdf6a0f68c6b3c6dffb9
Maksym <mdundukov@gmail.com>
2021-07-03 17:11:17 +0300

    add func for unfavorite btn

--- src/app/favorite-page/favorite-page.component.ts
+++ src/app/favorite-page/favorite-page.component.ts
@@ -8,6 +8,7 @@ import {Router} from "@angular/router";
 })
 export class FavoritePageComponent implements OnInit {
   public favoriteListMovies: any[] = [];
+  public deleteFilmId: any;
   public imgSrc: string = 'https://image.tmdb.org/t/p/w300';
 
   constructor(private router: Router) {
@@ -26,5 +27,12 @@ export class FavoritePageComponent implements OnInit {
       this.router.navigate(['/detail', e.target.id])
     }
     return
+  }
+
+  unFavoriteFilm(e: any): void {
+    this.deleteFilmId = this.favoriteListMovies.find((m: any) => m.id === Number(e.target.id));
+    let index = this.favoriteListMovies.indexOf(this.deleteFilmId)
+    this.favoriteListMovies.splice(index, 1)
+    localStorage.setItem('favorite', JSON.stringify(this.favoriteListMovies));
   }
 }
