3338ab9c9f33f03595255cecd2fd11b61fbb075b
Maksym <mdundukov@gmail.com>
2021-07-03 19:07:56 +0300

    add new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {MainPageComponent} from "./main-page/main-page.component";
 import {FavoritePageComponent} from "./favorite-page/favorite-page.component";
 import {DetailPageComponent} from "./detail-page/detail-page.component";
+import {FavoriteDetailComponent} from "./favorite-detail/favorite-detail.component";
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
   {path: 'favorite', component: FavoritePageComponent},
-  {path: 'detail/:id', component: DetailPageComponent}
+  {path: 'detail/:id', component: DetailPageComponent},
+  {path: 'favorite-detail/:id', component: FavoriteDetailComponent},
 ];
 
 @NgModule({
