09d352a76f76b680ae6a6798fd28a23f00652e04
Maksym <mdundukov@gmail.com>
2021-07-04 16:40:46 +0300

    add pagination

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -12,6 +12,7 @@ export class MainPageComponent implements OnInit {
   public totalDb: any = [];
   public imgUrl: string = environment.imgSrc;
   public totalPages: any;
+  public paginArray: any[] = [1, 2, 3, 4, 5];
 
   constructor(private router: Router, private dataBase: DataService,) {
   }
@@ -38,26 +39,52 @@ export class MainPageComponent implements OnInit {
   nextPage(): void {
     this.dataBase.currentPage++;
     this.loadDataMovies(this.dataBase.currentPage);
+    this.upgradePagination();
   }
 
   firstPage(): void {
     this.dataBase.currentPage = 1;
     this.loadDataMovies(this.dataBase.currentPage);
+    this.upgradePagination();
   }
 
   prevPage(): void {
     if (this.dataBase.currentPage >= 2) {
       this.dataBase.currentPage--;
       this.loadDataMovies(this.dataBase.currentPage);
+      this.upgradePagination();
     }
   }
 
-  paginBtn(): void {
+  upgradePagination() {
+    if (this.dataBase.currentPage + 4 <= this.totalPages.length) {
+      for (let i = this.dataBase.currentPage; i <= this.dataBase.currentPage + 5; i++) {
+        this.paginArray.push(i);
+        this.paginArray.shift(i);
+      }
+      for (let i = this.dataBase.currentPage; i >= this.dataBase.currentPage - 2; i--) {
+        this.paginArray.pop(i);
+        this.paginArray.unshift(i);
+      }
+    } else {
+      for (let i = this.dataBase.currentPage; i >= this.dataBase.currentPage - 4; i--) {
+        this.paginArray.pop(i);
+        this.paginArray.unshift(i);
+      }
+      for (let i = this.dataBase.currentPage; i <= this.dataBase.currentPage - 4; i++) {
+        this.paginArray.push(i);
+        this.paginArray.shift(i);
+      }
+    }
+  }
+
+  paginBtn(e: any): void {
 
   }
 
   lastPage(): void {
     this.dataBase.currentPage = this.totalPages;
     this.loadDataMovies(this.dataBase.currentPage);
+    this.upgradePagination();
   }
 }
